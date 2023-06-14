edb6b744d0dee31248c5ae6cef83b01692ea29a2
Neil <nr.brand@yahoo.com>
2023-06-14 17:08:48 -0400

    Implimented useparams to grab name of single pokemon

--- pokemonapp/src/pages/PokeSingle.jsx
+++ pokemonapp/src/pages/PokeSingle.jsx
@@ -14,15 +14,20 @@ import {
 import { useQuery } from "@apollo/client";
 import { GET_SINGLE_POKEMON } from "../components/graphQL/queries";
 import { useEffect } from "react";
+import { useParams } from "react-router";
 
 // TODO: Grab stats from api based on name. See:https://pokeapi.co/docs/v2#pokemon-section
 
-function PokeSingle({}) {
+function PokeSingle() {
   const [pokeData, setPokeData] = useState();
+
+  // get state from react router
+  let { name } = useParams();
+  console.log("name outside", name);
 
   const { loading, error, data } = useQuery(GET_SINGLE_POKEMON, {
     variables: {
-      name: "ivysaur",
+      name: name,
     },
   });
 
@@ -30,8 +35,9 @@ function PokeSingle({}) {
     if (loading) {
       console.log("Loading");
     } else {
-      console.log("Data SINGLE:", data);
-      setPokeData(data.pokemon.pokemon);
+      console.log("Data SINGLE:", data.pokemon);
+      setPokeData(data.pokemon);
+      console.log("name", name);
     }
 
     if (error) {
@@ -43,11 +49,26 @@ function PokeSingle({}) {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle></IonTitle>
+          <IonTitle style={{ textTransform: "Capitalize" }}>{name}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonTitle size="large">POKE SINGLE</IonTitle>
+        {!pokeData ? (
+          <IonItem>
+            <IonLabel>LOADING</IonLabel>
+            <IonSpinner name="dots"></IonSpinner>
+          </IonItem>
+        ) : (
+          <>
+            <IonTitle size="large" style={{ textTransform: "Capitalize" }}>
+              {pokeData?.name}
+            </IonTitle>
+            <IonTitle size="large" style={{ textTransform: "Capitalize" }}>
+              Type: {pokeData?.types[0].type.name}
+            </IonTitle>
+            <IonTitle size="large">Height: {pokeData?.height}</IonTitle>
+          </>
+        )}
       </IonContent>
     </IonPage>
   );
